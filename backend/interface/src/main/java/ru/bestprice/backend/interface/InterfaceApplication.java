f091ad36bc6c94d108bbd527aa06af21d0e6b1f5
Д C Ж <jads@src.local>
2019-06-21 13:22:15 +0300

    add interface project

---
+++ backend/interface/src/main/java/ru/bestprice/backend/interface/InterfaceApplication.java
@@ -0,0 +1,13 @@
+package ru.bestprice.backend.interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceApplication.class, args);
+	}
+
+}

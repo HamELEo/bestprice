172054db0f07521b6fdcdb4ab87dc9f74224c38a
Д C Ж <jads@src.local>
2019-06-21 13:22:25 +0300

    add elastic project

---
+++ backend/elastic/src/main/java/ru/bestprice/backend/elastic/ElasticApplication.java
@@ -0,0 +1,13 @@
+package ru.bestprice.backend.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticApplication.class, args);
+	}
+
+}

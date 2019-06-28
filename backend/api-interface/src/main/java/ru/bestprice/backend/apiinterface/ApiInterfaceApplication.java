0a757d643d1d36c0345d4fb1d02a42994e555a02
Д C Ж <jads@src.local>
2019-06-28 11:18:35 +0300

    add controller

--- backend/api-interface/src/main/java/ru/bestprice/backend/apiinterface/ApiInterfaceApplication.java
+++ backend/api-interface/src/main/java/ru/bestprice/backend/apiinterface/ApiInterfaceApplication.java
@@ -3,6 +3,8 @@ package ru.bestprice.backend.apiinterface;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class ApiInterfaceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiInterfaceApplication.class, args);
+	}
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
---
+++ backend/api-interface/src/main/java/ru/bestprice/backend/apiinterface/controller/ApiController.java
@@ -0,0 +1,53 @@
+package ru.bestprice.backend.apiinterface.controller;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@RestController
+public class ApiController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Qualifier("eurekaClient")
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @Value("${service.selenium.serviceId}")
+    private String esIdService;
+
+    @Value("${service.selenium.linkSearch}")
+    private String esSearchLink;
+
+    @PostMapping(value = "qrcode", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getQrCode(@RequestBody Map<String, String> body) {
+        System.out.println(body);
+        Application application = eurekaClient.getApplication(esIdService);
+        InstanceInfo instanceInfo = application.getInstances().get(0);
+        String url= "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort() + "/" + esSearchLink;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(body, headers);
+        try {
+            ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+            if (exchange.getStatusCodeValue() == 200) {
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getRawStatusCode()).headers(e.getResponseHeaders()).body(e.getResponseBodyAsString());
+        }
+    }
+}

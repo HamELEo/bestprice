8c16e9ba03598a29870ca67fc95acdfa2cbefad2
Д C Ж <jads@src.local>
2019-07-01 10:26:24 +0300

    update parse QR-Code

--- backend/selenium/src/main/java/ru/bestprice/backend/selenium/controller/SeleniumController.java
+++ backend/selenium/src/main/java/ru/bestprice/backend/selenium/controller/SeleniumController.java
@@ -1,27 +1,22 @@
 package ru.bestprice.backend.selenium.controller;
 
 import com.netflix.discovery.EurekaClient;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Proxy;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
-import ru.bestprice.backend.selenium.entity.QrCode.QrCode;
+import ru.bestprice.backend.selenium.entity.QrCode;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.*;
 
 @Controller
@@ -50,13 +45,27 @@ public class SeleniumController {
 
     private ChromeOptions options;
 
+    /* @PostMapping(value = "set_qr_code")
+     public ResponseEntity<?> setQrCode(@RequestBody Map<String, String> body) {
+         Properties systemProperties = System.getProperties();
+         systemProperties.setProperty( "http.proxyHost", "192.168.254.20" );
+         systemProperties.setProperty( "http.proxyPort", "3142" );
+         systemProperties.setProperty( "https.proxyHost", "192.168.254.20" );
+         systemProperties.setProperty( "https.proxyPort", "3142" );
+         QrCode code = parseQrCode(body.get("code"));
+         HttpEntity<String> entity = new HttpEntity<>(createHeaders("+79104266111", "723711"));
+         ResponseEntity<String> exchange = restTemplate.exchange("https://proverkacheka.nalog.ru:9999/v1/mobile/users/login", HttpMethod.GET, entity, String.class);
+         System.out.println(exchange.getStatusCodeValue());
+         return new ResponseEntity<>(HttpStatus.OK);
+     }*/
+
     @PostMapping(value = "set_qr_code")
     public ResponseEntity<?> qrCode(@RequestBody Map<String, String> body) {
         Properties systemProperties = System.getProperties();
-        systemProperties.setProperty( "http.proxyHost", "192.168.254.20" );
-        systemProperties.setProperty( "http.proxyPort", "3142" );
-        systemProperties.setProperty( "https.proxyHost", "192.168.254.20" );
-        systemProperties.setProperty( "https.proxyPort", "3142" );
+        systemProperties.setProperty("http.proxyHost", "192.168.254.20");
+        systemProperties.setProperty("http.proxyPort", "3142");
+        systemProperties.setProperty("https.proxyHost", "192.168.254.20");
+        systemProperties.setProperty("https.proxyPort", "3142");
         QrCode code = parseQrCode(body.get("code"));
         MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
         requestBody.put("fn", Arrays.asList(code.getFn()));
@@ -65,7 +74,7 @@ public class SeleniumController {
         requestBody.put("n", Arrays.asList(code.getN()));
         requestBody.put("s", Arrays.asList(code.getS()));
         requestBody.put("t", Arrays.asList(code.getT()));
-        requestBody.put("qr",Arrays.asList( "0"));
+        requestBody.put("qr", Arrays.asList("0"));
         String uri = "https://proverkacheka.com/check/get".trim();
         HttpHeaders headers = new HttpHeaders();
         headers.set("Cookie", "ENGID=1.1; _ym_uid=1560243443710875796; _ym_d=1560243443; _ym_isad=2");
@@ -116,6 +125,35 @@ public class SeleniumController {
 
     private QrCode parseQrCode(String qrCode) {
         QrCode code = new QrCode();
+        qrCode = qrCode.replaceAll("&", "\n");
+        ByteArrayInputStream stream = new ByteArrayInputStream(qrCode.getBytes());
+        Properties properties = new Properties();
+        try {
+            properties.load(stream);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+        code.setFd(properties.getProperty("i"));
+        code.setFn(properties.getProperty("fn"));
+        code.setFp(properties.getProperty("fp"));
+        code.setS(properties.getProperty("s").replaceAll("\\.", ""));
+        code.setN(properties.getProperty("n"));
+        code.setT(properties.getProperty("t"));
+        code.setQr("0");
+        return code;
+    }
+
+    private HttpHeaders createHeaders(String username, String password) {
+        String auth = username + ":" + password;
+        byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes());
+        String authHeader = "Basic " + new String(encodedAuth);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", authHeader);
+        return headers;
+    }
+
+   /* private QrCode parseQrCode(String qrCode) {
+        QrCode code = new QrCode();
         Map<String, String> map = new LinkedHashMap<>();
         String[] split = qrCode.split("&");
         for (String t : split) {
@@ -137,7 +175,7 @@ public class SeleniumController {
         code.setT(dateTime);
         code.setQr("0");
         return code;
-    }
+    }*/
 
    /* private synchronized ChromeOptions getChromeOptions() {
         if (options == null) {
